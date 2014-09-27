2c2774099602075ade7e19891033c3307b8e0f0f
Jeremy D. Frens <jdfrens@gmail.com>
2014-09-27 18:32:03 -0500

    [Haskell] Use defaultMain for the test runner.

--- Haskell/Tests.hs
+++ Haskell/Tests.hs
@@ -1,13 +1,7 @@
 module Main (main) where
 
-import System.Exit
+import Test.Framework (defaultMain)
 
-import Test.HUnit
 import OptionsTest
 
-main :: IO ()
-main = do
-    counts2 <- runTestTT optionsModuleTests
-    if (errors counts2 + failures counts2 == 0)
-        then exitSuccess
-        else exitFailure
+main = defaultMain optionsModuleTests
