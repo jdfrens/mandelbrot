8cbd16edf3693de5d668e31167b36bcf3d0f7483
Jeremy D. Frens <jdfrens@gmail.com>
2015-04-23 14:15:08 -0500

    Add burning-ship fractal. #elixir

--- elixir/lib/mandelbrot/next_function.ex
+++ elixir/lib/mandelbrot/next_function.ex
@@ -2,14 +2,24 @@ defmodule Mandelbrot.NextFunction do
 
   def next_function(options) do
     case options.fractal do
-      :mandelbrot -> &mandelbrot_next/2
+      :burningship -> &burning_ship_next/2
+      :mandelbrot  -> &mandelbrot_next/2
     end
   end
 
+  import Complex, only: [ square: 1, add: 2 ]
+
   def mandelbrot_next(z, c) do
-    z
-    |> Complex.square()
-    |> Complex.add(c)
+    z |> square() |> add(c)
+  end
+
+  def burning_ship_next(z, c) do
+    burn(z) |> square() |> add(c)
+  end
+
+  def burn(%Complex{ real: real, imag: imag }) do
+    # FIXME: not sure why I need to negate imag since Wikipedia doesn't
+    %Complex{ real: abs(real), imag: -1 * abs(imag) }
   end
 
 end
--- elixir/test/mandelbrot/next_function_test.exs
+++ elixir/test/mandelbrot/next_function_test.exs
@@ -8,4 +8,19 @@ defmodule Mandelbrot.NextFunction.Test do
     assert %Complex{ real: 6.0, imag: 13.0 } == Mandelbrot.NextFunction.mandelbrot_next(z, c)
   end
 
+  test "burning_ship_next" do
+    z = %Complex{ real: 3.0, imag: 2.0 }
+    c = %Complex{ real: 1.0, imag: 1.0 }
+    assert %Complex{ real: 6.0, imag: -11.0 } == Mandelbrot.NextFunction.burning_ship_next(z, c)
+  end
+
+  test "burn" do
+    import Mandelbrot.NextFunction, only: [ burn: 1 ]
+
+    assert %Complex{ real: 5.0, imag: -3.0 } == burn(%Complex{ real:  5.0, imag:  3.0 })
+    assert %Complex{ real: 5.0, imag: -3.0 } == burn(%Complex{ real:  5.0, imag: -3.0 })
+    assert %Complex{ real: 5.0, imag: -3.0 } == burn(%Complex{ real: -5.0, imag:  3.0 })
+    assert %Complex{ real: 5.0, imag: -3.0 } == burn(%Complex{ real: -5.0, imag: -3.0 })
+  end
+
 end
