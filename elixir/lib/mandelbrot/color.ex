c03e315a30b9fac304ee61d8ae81d4a124db9bc0
Jeremy D. Frens <jdfrens@gmail.com>
2015-04-23 14:27:39 -0500

    Add green, red, and (stupid) random color schemes. #elixir

--- elixir/lib/mandelbrot/color.ex
+++ elixir/lib/mandelbrot/color.ex
@@ -1,9 +1,14 @@
 defmodule Mandelbrot.Color do
 
+  # FIXME: DRY it up!
   @max_intensity 255
+  @max_iterations  256
+  @half_iterations 127
 
-  @black :io_lib.format("~3B ~3B ~3B ", [0, 0, 0])
+  @black :io_lib.format("~3B ~3B ~3B ", [  0,   0,   0])
+  @white :io_lib.format("~3B ~3B ~3B ", [255, 255, 255])
   def black, do: @black
+  def white, do: @white
 
   # TODO: blog about this
   def ppm(red, green, blue) do
@@ -12,19 +17,34 @@ defmodule Mandelbrot.Color do
 
   def color_function(options) do
     case options.color do
-      :blue -> &Mandelbrot.Color.scaled_blue/1
+      :blue   -> &scaled_blue/1
+      :green  -> &scaled_green/1
+      :red    -> &scaled_red/1
+      :random ->
+        # FIXME: :random is wrong
+        # -- need to figure out how to generate random numbers well in Elixir
+        # -- need to memoize the random colors
+        &scaled_blue/1
     end
   end
 
   def scaled_blue(pixel) do
     pov_scale(pixel,
-      &(ppm(0, 0, &1)),
+      &(ppm( 0,  0,             &1)),
       &(ppm(&1, &1, @max_intensity)))
   end
 
-  # FIXME: DRY it up!
-  @max_iterations  256
-  @half_iterations 127
+  def scaled_green(pixel) do
+    pov_scale(pixel,
+      &(ppm( 0,             &1,  0)),
+      &(ppm(&1, @max_intensity, &1)))
+  end
+
+  def scaled_red(pixel) do
+    pov_scale(pixel,
+      &(ppm(            &1,  0,  0)),
+      &(ppm(@max_intensity, &1, &1)))
+  end
 
   # FIXME: pick a better name
   def pov_scale({ :inside,  _, _ }, _, _), do: @black
--- elixir/test/mandelbrot/color_test.exs
+++ elixir/test/mandelbrot/color_test.exs
@@ -6,21 +6,57 @@ defmodule Mandelbrot.Color.Test do
     assert "  0   0 255 " == Mandelbrot.Color.ppm(0, 0, 255)
   end
 
-  test "scaled_blue" do
-    import Mandelbrot.Color, only: [ black: 0, scaled_blue: 1, ppm: 3 ]
+  test "pov_scale" do
+    import Mandelbrot.Color, only: [ black: 0, pov_scale: 3 ]
 
-    assert black() == scaled_blue({:inside, :meh, :meh})
+    assert black() == pov_scale({:inside, :meh, :meh}, :meh, :meh)
+
+    plateau = &("plateau #{&1}")
+    border = &("border #{&1}")
 
     # FIXME: what about 0 iterations?
-    assert ppm(  0,   0,   0) == scaled_blue({ :outside, :meh,   1 })
-    assert ppm(  0,   0,   2) == scaled_blue({ :outside, :meh,   2 })
+    assert "plateau 0" == pov_scale({ :outside, :meh,   1 }, plateau, border)
+    assert "plateau 2" == pov_scale({ :outside, :meh,   2 }, plateau, border)
 
-    assert ppm(  0,   0, 249) == scaled_blue({ :outside, :meh, 126 })
+    assert "plateau 249" == pov_scale({ :outside, :meh, 126 }, plateau, border)
+    assert "plateau 251" == pov_scale({ :outside, :meh, 127 }, plateau, border)
+    assert "border 0" == pov_scale({ :outside, :meh, 128 }, plateau, border)
+    assert "border 2" == pov_scale({ :outside, :meh, 129 }, plateau, border)
+
+    assert "border 253" == pov_scale({ :outside, :meh, 255 }, plateau, border)
+    assert "border 255" == pov_scale({ :outside, :meh, 256 }, plateau, border)
+  end
+
+  test "scaled_blue" do
+    import Mandelbrot.Color, only: [ scaled_blue: 1, ppm: 3 ]
+
+    assert ppm(  0,   0,   0) == scaled_blue({ :outside, :meh,   1 })
     assert ppm(  0,   0, 251) == scaled_blue({ :outside, :meh, 127 })
     assert ppm(  0,   0, 255) == scaled_blue({ :outside, :meh, 128 })
-    assert ppm(  2,   2, 255) == scaled_blue({ :outside, :meh, 129 })
-
-    assert ppm(253, 253, 255) == scaled_blue({ :outside, :meh, 255 })
     assert ppm(255, 255, 255) == scaled_blue({ :outside, :meh, 256 })
+  end
+
+  test "scaled_green" do
+    import Mandelbrot.Color, only: [ scaled_green: 1, ppm: 3 ]
+
+    assert ppm(  0,   0,   0) == scaled_green({ :outside, :meh,   1 })
+    assert ppm(  0, 251,   0) == scaled_green({ :outside, :meh, 127 })
+    assert ppm(  0, 255,   0) == scaled_green({ :outside, :meh, 128 })
+    assert ppm(255, 255, 255) == scaled_green({ :outside, :meh, 256 })
+  end
+
+  test "scaled_red" do
+    import Mandelbrot.Color, only: [ scaled_red: 1, ppm: 3 ]
+
+    assert ppm(  0,   0,   0) == scaled_red({ :outside, :meh,   1 })
+    assert ppm(251,   0,   0) == scaled_red({ :outside, :meh, 127 })
+    assert ppm(255,   0,   0) == scaled_red({ :outside, :meh, 128 })
+    assert ppm(255, 255, 255) == scaled_red({ :outside, :meh, 256 })
+  end
+
+  test "random" do
+    import Mandelbrot.Color, only: [ white: 0, random_color: 1, ppm: 3 ]
+
+    assert white == random_color({ :insider, :meh, :meh })
   end
 end
