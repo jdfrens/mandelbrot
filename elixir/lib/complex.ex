41e7bfe435dfc3c2bd8f455635b8ad1f13a073b3
Jeremy D. Frens <jdfrens@gmail.com>
2015-06-06 18:17:27 -0500

    Pattern match complex numbers in parameter list. #elixir

--- elixir/lib/complex.ex
+++ elixir/lib/complex.ex
@@ -1,6 +1,10 @@
 defmodule Complex do
+
   defstruct [ :real, :imag ]
 
+  @type t :: %Complex{ real: float, imag: float }
+
+  @spec parse(String.t) :: Complex.t
   def parse(str) do
     [_, real, imag] = Regex.run(~r/([-]?\d+\.\d+)\+([-]?\d+\.\d+)i/, str)
     %Complex{
@@ -9,22 +13,27 @@ defmodule Complex do
     }
   end
 
+  @spec magnitude(Complex.t) :: float
   def magnitude(z) do
     magnitude_squared(z) |> :math.sqrt
   end
 
+  @spec magnitude_squared(Complex.t) :: float
   def magnitude_squared(%Complex{real: real, imag: imag}) do
     real * real + imag * imag
   end
 
+  @spec add(Complex.t, Complex.t) :: Complex.t
   def add(%Complex{real: a, imag: b}, %Complex{real: c, imag: d}) do
     %Complex { real: a + c, imag: b + d }
   end
 
+  @spec subtract(Complex.t, Complex.t) :: Complex.t
   def subtract(%Complex{real: a, imag: b}, %Complex{real: c, imag: d}) do
     %Complex { real: a - c, imag: b - d }
   end
 
+  @spec multiply(Complex.t, Complex.t) :: Complex.t
   def multiply(%Complex{real: a, imag: b}, %Complex{real: c, imag: d}) do
     %Complex{
       real: a * c - b * d,
@@ -32,6 +41,7 @@ defmodule Complex do
     }
   end
 
+  @spec square(Complex.t) :: Complex.t
   def square(%Complex{real: a, imag: b}) do
     %Complex{
       real: a * a - b * b,
@@ -39,10 +49,12 @@ defmodule Complex do
     }
   end
 
+  @spec cube(Complex.t) :: Complex.t
   def cube(z) do
     z |> square() |> multiply(z)
   end
 
+  @spec divide(Complex.t, Complex.t) :: Complex.t
   def divide(z0, z1) do
     denominator = z1.real * z1.real + z1.imag * z1.imag
     %Complex{
