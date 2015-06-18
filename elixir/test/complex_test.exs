4d1b70b8b1d980362c260e90fd6f38ec56b4372a
Jeremy D. Frens <jdfrens@gmail.com>
2015-06-18 13:09:36 -0500

    Use pavlov for #elixir tests.

--- elixir/test/complex_test.exs
+++ elixir/test/complex_test.exs
@@ -1,40 +1,59 @@
 defmodule Complex.Test do
 
-  use ExUnit.Case, async: true
+  use Pavlov.Case, async: true
+  import Pavlov.Syntax.Expect
 
-  test "parse" do
-    assert %Complex{ real:  1.1, imag:  2.2 } == Complex.parse("1.1+2.2i")
-    assert %Complex{ real: -1.1, imag: -2.2 } == Complex.parse("-1.1+-2.2i")
+	describe ".parse" do
+		it "parses" do
+			expect Complex.parse("1.1+2.2i") |> to_eq %Complex{ real:  1.1, imag:  2.2 }
+		end
+		it "parses negative numbers" do
+			expect Complex.parse("-1.1+-2.2i") |> to_eq %Complex{ real: -1.1, imag: -2.2 }
+		end
+	end
+
+	describe ".add" do
+		it "adds" do
+			z0 = %Complex{ real: 5.0, imag: 12.0 }
+			z1 = %Complex{ real: 3.0, imag:  2.0 }
+			expect Complex.add(z0, z1) |> to_eq %Complex{ real: 8.0, imag: 14.0 }
+		end
+	end
+
+  describe ".subtract" do
+		it "subtracts" do
+			z0 = %Complex{ real: 5.0, imag: 12.0 }
+			z1 = %Complex{ real: 3.0, imag:  2.0 }
+			expect Complex.subtract(z0, z1) |> to_eq %Complex{ real: 2.0, imag: 10.0 }
+		end
   end
 
-  test "add" do
-    z0 = %Complex{ real: 5.0, imag: 12.0 }
-    z1 = %Complex{ real: 3.0, imag:  2.0 }
-    assert %Complex{ real: 8.0, imag: 14.0 } == Complex.add(z0, z1)
+  describe ".multiply" do
+		it "multiplies" do
+			z0 = %Complex{ real: 5.0, imag: 12.0 }
+			z1 = %Complex{ real: 3.0, imag:  2.0 }
+			expect Complex.multiply(z0, z1) |> to_eq %Complex{ real: -9.0, imag: 46.0 }
+		end
   end
 
-  test "subtract" do
-    z0 = %Complex{ real: 5.0, imag: 12.0 }
-    z1 = %Complex{ real: 3.0, imag:  2.0 }
-    assert %Complex{ real: 2.0, imag: 10.0 } == Complex.subtract(z0, z1)
+  describe ".square" do
+		it "squares" do
+			expect Complex.square(%Complex{ real: 3.0, imag: 2.0 })
+			|> to_eq %Complex{ real: 5.0, imag: 12.0 }
+		end
+	end
+
+  describe ".cube" do
+		it "cubes" do
+			expect Complex.cube(%Complex{ real: 3.0, imag: 2.0 })
+			|> to_eq %Complex{ real: -9.0, imag: 46.0 }
+		end
   end
 
-  test "multiply" do
-    z0 = %Complex{ real: 5.0, imag: 12.0 }
-    z1 = %Complex{ real: 3.0, imag:  2.0 }
-    assert %Complex{ real: -9.0, imag: 46.0 } == Complex.multiply(z0, z1)
-  end
-
-  test "square" do
-    assert %Complex{ real: 5.0, imag: 12.0 } == Complex.square(%Complex{ real: 3.0, imag: 2.0 })
-  end
-
-  test "cube" do
-    assert %Complex{ real: -9.0, imag: 46.0 } == Complex.cube(%Complex{ real: 3.0, imag: 2.0 })
-  end
-
-  test "magnitude" do
-    assert 5.0 = Complex.magnitude(%Complex{ real: 3.0, imag: 4.0 })
+  describe ".magnitude" do
+    it "squares and square roots" do
+			expect Complex.magnitude(%Complex{ real: 3.0, imag: 4.0 }) |> to_eq 5.0
+		end
   end
 
 end
