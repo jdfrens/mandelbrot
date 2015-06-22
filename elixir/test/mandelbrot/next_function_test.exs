738e6194d0b371d8267900f50e91b5e7328ee05e
Jeremy D. Frens <jdfrens@gmail.com>
2015-06-22 14:39:11 -0500

    Translate "next function" tests to Pavlov. #elixir

--- elixir/test/mandelbrot/next_function_test.exs
+++ elixir/test/mandelbrot/next_function_test.exs
@@ -1,37 +1,56 @@
 defmodule Mandelbrot.NextFunction.Test do
 
-  use ExUnit.Case, async: true
+  use Pavlov.Case, async: true
+	import Pavlov.Syntax.Expect
 
-  import Mandelbrot.NextFunction, only: [ mandelbrot_next: 1, julia_next: 1, burning_ship_next: 1 ]
+  describe ".mandelbrot_next" do
+		import Mandelbrot.NextFunction, only: [ mandelbrot_next: 1 ]
 
-  test "mandelbrot_next" do
-    z = %Complex{ real: 3.0, imag: 2.0 }
-    c = %Complex{ real: 1.0, imag: 1.0 }
-    assert %Complex{ real: 6.0, imag: 13.0 } == mandelbrot_next(c).(z)
+		it "squares and adds" do
+			z = %Complex{ real: 3.0, imag: 2.0 }
+			c = %Complex{ real: 1.0, imag: 1.0 }
+			expect(mandelbrot_next(c).(z)) |> to_eq %Complex{ real: 6.0, imag: 13.0 }
+		end
   end
 
-  test "julia_next" do
-    z = %Complex{ real: 3.0, imag: 2.0 }
-    c = %Complex{ real: 1.0, imag: 1.0 }
-    assert %Complex{ real: 6.0, imag: 13.0 } == julia_next(c).(z)
+  describe ".julia_next" do
+		import Mandelbrot.NextFunction, only: [ julia_next: 1 ]
+
+		it "squares and adds" do
+			z = %Complex{ real: 3.0, imag: 2.0 }
+			c = %Complex{ real: 1.0, imag: 1.0 }
+			expect julia_next(c).(z) |> to_eq %Complex{ real: 6.0, imag: 13.0 }
+		end
   end
 
-  test "burning_ship_next" do
-    z = %Complex{ real: 3.0, imag: 2.0 }
-    c = %Complex{ real: 1.0, imag: 1.0 }
-    assert %Complex{ real: 6.0, imag: -11.0 } == burning_ship_next(c).(z)
+  describe ".burning_ship_next" do
+		import Mandelbrot.NextFunction, only: [ burning_ship_next: 1 ]
+
+		it "burns and squares and adds" do
+			z = %Complex{ real: 3.0, imag: 2.0 }
+			c = %Complex{ real: 1.0, imag: 1.0 }
+			burning_ship_next(c).(z) |> to_eq %Complex{ real: 6.0, imag: -11.0 }
+		end
+
+		describe ".burn" do
+			import Mandelbrot.NextFunction, only: [ burn: 1 ]
+
+			it "negates the imaginary component" do
+				expect burn(%Complex{ real:  5.0, imag:  3.0 })
+				|> to_eq %Complex{ real: 5.0, imag: -3.0 }
+			end
+			it "keeps the imaginary component negative" do
+				burn(%Complex{ real:  5.0, imag: -3.0 })
+				|> to_eq %Complex{ real: 5.0, imag: -3.0 }
+			end
+			it "uses the absolute value of the real component" do
+				burn(%Complex{ real: -5.0, imag:  3.0 })
+				|> to_eq %Complex{ real: 5.0, imag: -3.0 }
+			end
+		end
   end
 
-  test "burn" do
-    import Mandelbrot.NextFunction, only: [ burn: 1 ]
-
-    assert %Complex{ real: 5.0, imag: -3.0 } == burn(%Complex{ real:  5.0, imag:  3.0 })
-    assert %Complex{ real: 5.0, imag: -3.0 } == burn(%Complex{ real:  5.0, imag: -3.0 })
-    assert %Complex{ real: 5.0, imag: -3.0 } == burn(%Complex{ real: -5.0, imag:  3.0 })
-    assert %Complex{ real: 5.0, imag: -3.0 } == burn(%Complex{ real: -5.0, imag: -3.0 })
-  end
-
-  test "newton" do
+  describe ".newton" do
 
   end
 end
