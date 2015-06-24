8b63e4d439af65db1cce1fedd4015208be73d67c
Jeremy D. Frens <jdfrens@gmail.com>
2015-06-24 16:28:46 -0500

    Translate fractal tests to Pavlov. #elixir

--- elixir/test/mandelbrot/fractal_test.exs
+++ elixir/test/mandelbrot/fractal_test.exs
@@ -1,51 +1,70 @@
 defmodule Mandelbrot.Fractal.Test do
 
-  use ExUnit.Case, async: true
+  use Pavlov.Case, async: true
+	import Pavlov.Syntax.Expect
 
-  test "generate_header" do
-    options = %Mandelbrot.Options{ size: %Mandelbrot.Size{ width: 55, height: 99 } }
-    assert ["P3", "55", "99", "255"] == Mandelbrot.Fractal.generate_header(options)
+  describe ".generate_header" do
+		it "spits out a PPM header including width and height" do
+			options = %Mandelbrot.Options{ size: %Mandelbrot.Size{ width: 55, height: 99 } }
+			expect Mandelbrot.Fractal.generate_header(options) |> to_eq ["P3", "55", "99", "255"]
+		end
   end
 
-  test "generate" do
+  describe ".generate" do
     # more of a property test
     import Mandelbrot.Fractal, only: [ generate: 1 ]
 
-    options       = %Mandelbrot.Options{
-      fractal:     :mandelbrot,
-      color:       :blue,
-      size:        %Mandelbrot.Size{ width: 30, height: 20 },
-      upper_left:  %Complex{ real: -2.0, imag:  1.0 },
-      lower_right: %Complex{ real:  1.0, imag: -1.0 }
-    }
+		it "generates an image" do
+			options       = %Mandelbrot.Options{
+				fractal:     :mandelbrot,
+				color:       :blue,
+				size:        %Mandelbrot.Size{ width: 30, height: 20 },
+				upper_left:  %Complex{ real: -2.0, imag:  1.0 },
+				lower_right: %Complex{ real:  1.0, imag: -1.0 }
+																 }
 
-    ppm = Mandelbrot.Fractal.generate(options) |> Enum.to_list
-    # 4 lines of header + 30 columns * 20 rows
-    assert 604 = Enum.count(ppm)
+			ppm = Mandelbrot.Fractal.generate(options) |> Enum.to_list
+			# 4 lines of header + 30 columns * 20 rows
+			expect Enum.count(ppm) |> to_eq 604
+		end
   end
 
-  test "fractal_iterate" do
+  describe ".fractal_iterate" do
     import Mandelbrot.Fractal, only: [ fractal_iterate: 3 ]
 
-    next = fn z -> z + 1 end
-    cutoff = fn z -> z < 0 end
-    assert { 1, 0 } == fractal_iterate(next, cutoff, 1)
+		let :next do
+			fn z -> z + 1 end
+		end
 
-    cutoff = fn z -> z < 128 end
-    assert { 128, 127 } == fractal_iterate(next, cutoff, 1)
+		it "stops due to cutoff" do
+			cutoff = fn z -> z < 0 end
+			expect fractal_iterate(next, cutoff, 1) |> to_eq { 1, 0 }
+		end
 
-    cutoff = fn z -> z < 500 end
-    assert { 256, 255 } == fractal_iterate(next, cutoff, 1)
+		it "stops later because of cutoff" do
+			cutoff = fn z -> z < 128 end
+			expect fractal_iterate(next, cutoff, 1) |> to_eq { 128, 127 }
+		end
+
+		it "stops because of the number of iterations" do
+			cutoff = fn z -> z < 500 end
+			expect fractal_iterate(next, cutoff, 1) |> to_eq { 256, 255 }
+		end
   end
 
-  test "in_or_out" do
+  describe ".in_or_out" do
     import Mandelbrot.Fractal, only: [ in_or_out: 1 ]
 
-    assert { :inside,  :meh, 538 } == in_or_out({ :meh, 538 })
-    assert { :inside,  :meh, 256 } == in_or_out({ :meh, 256 })
-    assert { :inside,  :meh, 255 } == in_or_out({ :meh, 255 })
-    assert { :outside, :meh, 254 } == in_or_out({ :meh, 254 })
-    assert { :outside, :meh,  34 } == in_or_out({ :meh,  34 })
+		it "is inside when iterations are greater than or equal to 255" do
+			expect in_or_out({ :meh, 538 }) |> to_eq { :inside,  :meh, 538 }
+			expect in_or_out({ :meh, 256 }) |> to_eq { :inside,  :meh, 256 }
+			expect in_or_out({ :meh, 255 }) |> to_eq { :inside,  :meh, 255 }
+		end
+
+		it "is outside when iterations are less than 255" do
+			expect in_or_out({ :meh, 254 }) |> to_eq { :outside,  :meh, 254 }
+			expect in_or_out({ :meh,  34 }) |> to_eq { :outside,  :meh,  34 }
+		end
   end
 
 end
