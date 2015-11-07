df3a8f7b4ae7e53b9dff3a1955b09103e444fb2e
Jeremy D. Frens <jdfrens@gmail.com>
2015-11-07 17:25:19 -0600

    Rework CLI for new options. #elixir

--- elixir/lib/fractals.ex
+++ elixir/lib/fractals.ex
@@ -9,19 +9,34 @@ defmodule Fractals do
     |> take_action
   end
 
-  def take_action({_, [ options_filename, image_filename ], _}) do
-    {:ok, image_file} = File.open(image_filename, [:write])
-
-    options_filename
-    |> options
-    |> Fractals.Generator.generate
-    |> Stream.each(fn line -> IO.puts(image_file, line) end)
-    |> Stream.run
-
-    File.close(image_file)
+  def take_action({flags, [ options_filename, output_filename ], _}) do
+    options = Fractals.Options.parse(flags, options_filename, output_filename)
+    if options.fractal == :nova do
+      IO.puts "Nova fractal not supported."
+    else
+      elapsed_time = generate(options)
+      IO.puts "ran in #{elapsed_time} seconds"
+    end
   end
 
-  def options(options_filename) do
-    options_filename |> File.read! |> Fractals.Options.parse()
+  def generate(options) do
+    measure(fn ->
+      {:ok, image_file} = File.open(options.output_filename, [:write])
+
+      options
+      |> Fractals.Generator.generate
+      |> Stream.each(fn line -> IO.puts(image_file, line) end)
+      |> Stream.run
+
+      File.close(image_file)
+    end)
+  end
+
+
+  def measure(function) do
+    function
+    |> :timer.tc
+    |> elem(0)
+    |> Kernel./(1_000_000)
   end
 end
