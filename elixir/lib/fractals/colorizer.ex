948b1b1fa9759f5e2f4f8441e37d3297a57035b8
Jeremy D. Frens <jdfrens@gmail.com>
2016-06-25 17:15:03 -0500

    Complete simple colorizers. #elixir
    Three color schemes: black-on-white, white-on-black, gray.

--- elixir/lib/fractals/colorizer.ex
+++ elixir/lib/fractals/colorizer.ex
@@ -2,8 +2,32 @@ defmodule Fractals.Colorizer do
   # NOTE: this implements the very simple black-on-white coloring
   # TODO: write other colorizers and figure out a good naming scheme
 
-  def color_point({_, iterations}, _) when iterations >= 256 do
+  alias Fractals.Options
+
+  @maximum_iterations 256
+
+  defmacro escaped?(iterations) do
+    quote do
+      unquote(iterations) >= @maximum_iterations
+    end
+  end
+
+  def color_point({_, iterations}, %Options{color: :black_on_white}) when escaped?(iterations) do
     PPM.black
   end
-  def color_point(_, _), do: PPM.white
+  def color_point(_, %Options{color: :black_on_white}), do: PPM.white
+
+  def color_point({_, iterations}, %Options{color: :white_on_black}) when escaped?(iterations) do
+    PPM.white
+  end
+  def color_point(_, %Options{color: :white_on_black}), do: PPM.black
+
+  def color_point({_, iterations}, %Options{color: :gray}) when escaped?(iterations) do
+    PPM.black
+  end
+  def color_point({_, iterations}, %Options{color: :gray}) do
+    factor = :math.sqrt(iterations / @maximum_iterations)
+    intensity = round(@maximum_iterations * factor * factor)
+    PPM.ppm(intensity, intensity, intensity)
+  end
 end
--- elixir/spec/fractals/colorizer_spec.exs
+++ elixir/spec/fractals/colorizer_spec.exs
@@ -2,17 +2,51 @@ defmodule Fractals.ColorizerSpec do
   use ESpec, async: true
 
   alias Fractals.Colorizer
+  alias Fractals.Options
 
   let :z, do: nil
-  let :options, do: nil
 
   describe ".color_point" do
-    it "colors outside" do
-      expect(Colorizer.color_point({z, 512}, options)) |> to(eq PPM.black)
+    context "for black-on-white" do
+      let :options, do: %Options{color: :black_on_white}
+
+      it "colors outside" do
+        expect(Colorizer.color_point({z, 512}, options)) |> to(eq PPM.black)
+      end
+
+      it "colors inside" do
+        expect(Colorizer.color_point({z, 128}, options)) |> to(eq PPM.white)
+      end
     end
 
-    it "colors inside" do
-      expect(Colorizer.color_point({z, 128}, options)) |> to(eq PPM.white)
+    context "for white-on-black" do
+      let :options, do: %Options{color: :white_on_black}
+
+      it "colors outside" do
+        expect(Colorizer.color_point({z, 512}, options)) |> to(eq PPM.white)
+      end
+
+      it "colors inside" do
+        expect(Colorizer.color_point({z, 128}, options)) |> to(eq PPM.black)
+      end
+    end
+
+    context "for gray" do
+      let :options, do: %Options{color: :gray}
+
+      it "is black inside" do
+        expect(Colorizer.color_point({z, 512}, options)) |> to(eq PPM.black)
+      end
+      it "scales 0 to black" do
+        expect(Colorizer.color_point({z,   0}, options)) |> to(eq PPM.black)
+      end
+      it "scales 128 to 128" do
+        expect(Colorizer.color_point({z, 128}, options))
+        |> to(eq PPM.ppm(128, 128, 128))
+      end
+      it "is white after maximum iterations" do
+        expect(Colorizer.color_point({z, 255}, options)) |> to(eq PPM.white)
+      end
     end
   end
 end
