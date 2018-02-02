2f81e244c324f1908d7fdbaefbb0f57f0e265c75
Jeremy D. Frens <jdfrens@gmail.com>
2018-02-02 15:27:20 -0600

    Update to Elixir 1.6 and update some packages.

--- elixir/mix.exs
+++ elixir/mix.exs
@@ -2,18 +2,19 @@ defmodule Fractals.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :fractals,
-     version: "0.0.1",
-     elixir: "~> 1.3",
-     escript: [main_module: Fractals.CLI],
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :fractals,
+      version: "0.0.1",
+      elixir: "~> 1.3",
+      escript: [main_module: Fractals.CLI],
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
   end
 
   def application do
-    [applications: [:logger, :porcelain, :yaml_elixir],
-     mod: {Fractals, []}]
+    [applications: [:logger, :porcelain, :yaml_elixir], mod: {Fractals, []}]
   end
 
   defp deps do
