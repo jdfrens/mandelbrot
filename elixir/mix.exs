5c6eb551d44707c2afb18108a0a69f752f46a384
Jeremy D. Frens <jdfrens@gmail.com>
2018-09-03 18:59:48 -0700

    [elixir] switch to mogrify

--- elixir/mix.exs
+++ elixir/mix.exs
@@ -9,12 +9,13 @@ defmodule Fractals.Mixfile do
       escript: [main_module: Fractals.CLI],
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
   def application do
-    [applications: [:logger, :porcelain, :yaml_elixir], mod: {Fractals, []}]
+    [applications: [:logger, :yaml_elixir], mod: {Fractals, []}]
   end
 
   defp deps do
@@ -23,9 +24,15 @@ defmodule Fractals.Mixfile do
       {:dialyxir, "~> 0.5.0", only: [:dev, :test]},
       {:gen_stage, "~> 0.14"},
       {:inflex, "~> 1.10.0"},
-      {:porcelain, "~> 2.0"},
+      {:mogrify, "~> 0.6.1"},
       {:uuid, "~> 1.1"},
       {:yaml_elixir, "~> 2.1.0"}
+    ]
+  end
+
+  defp aliases do
+    [
+      all: ["test", "mix format --check-formatted", "credo --strict"]
     ]
   end
 end
